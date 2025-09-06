6202d6d8ee84272a4341596bf67dfd6fcc6fd3ca
elkhoudh <elkhoudh@gmail.com>
2025-09-06 17:31:34 -0400

    Able to start n8n locally

--- packages/@n8n/node-cli/scripts/copy-templates.mjs
+++ packages/@n8n/node-cli/scripts/copy-templates.mjs
@@ -3,9 +3,12 @@
 import glob from 'fast-glob';
 import { cp } from 'node:fs/promises';
 import path from 'path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const templateFiles = glob.sync(['src/template/templates/**/*'], {
-	cwd: path.resolve(import.meta.dirname, '..'),
+	cwd: path.resolve(__dirname, '..'),
 	ignore: ['**/node_modules', '**/dist'],
 	dot: true,
 });
